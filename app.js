58f8d7ec05066b363c61b82be9cc18f00cdb5dfc
Noer <noerpaanakker@hotmail.com>
2017-09-10 13:54:13 +0200

    added all images to all view, not responsive yet

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ app.set('views', 'public/views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index');
+})
+
+app.get('/nature', (req, res) => {
+	res.render('nature');
+})
+
+app.get('/animals', (req, res) => {
+	res.render('animals');
+})
+
+app.get('/backpackers', (req, res) => {
+	res.render('backpackers');
 })
 
 const server = app.listen(3000, () => {
